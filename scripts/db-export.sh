02908cb2bfc337f45fa2bdfc1616ee8954c10911
mono <monotom@gmail.com>
2016-03-08 07:59:34 +0100

    make it running

--- scripts/db-export.sh
+++ scripts/db-export.sh
@@ -4,4 +4,4 @@ container_context=${parentdir//[-_]/}
 container_name=$container_context"_db_1"
 
 database_name='wordpress'
-docker exec -d $container_name bash -c "mysqldump $database_name > /tmp/mysql/dump.sql"
+docker exec -d $container_name bash -c "mysqldump -uroot -pwordpress $database_name > /tmp/mysql/dump.sql"
