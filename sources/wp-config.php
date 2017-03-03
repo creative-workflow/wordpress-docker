31f2d69ef71e36d5493788db21ca5da28fed916c
mono <monotom@gmail.com>
2017-03-03 15:47:53 +0100

    update to docker-compose version 2, use maria db and create wordpress database by hand

--- sources/wp-config.php
+++ sources/wp-config.php
@@ -22,14 +22,15 @@
 /** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'wordpress');
-define('DB_HOST', "db:3306");
+define('DB_PASSWORD', '');
+define('DB_HOST', 'db');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 # define('WP_DEFAULT_THEME', 'cw-basic-theme');
-# define('WP_SITEURL', 'http://'.$_SERVER['SERVER_NAME'].( $_SERVER['SERVER_PORT'] != 80 ? ':'.$_SERVER['SERVER_PORT'] : ''));
-# define('WP_HOME', WP_SITEURL);
+
+define('WP_SITEURL', 'http://'.$_SERVER['HTTP_HOST'].( $_SERVER['SERVER_PORT'] != 80 ? ':'.$_SERVER['SERVER_PORT'] : ''));
+define('WP_HOME', WP_SITEURL);
 
 /**#@+
  * Authentication Unique Keys and Salts.
