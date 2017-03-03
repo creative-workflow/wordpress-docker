830ce124a4af10482e206935939b05e6d2ef3c5b
mono <monotom@gmail.com>
2017-03-03 15:46:36 +0100

    remove old scripts

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CONTAINERS=(db web php)
-for name in ${CONTAINERS[@]}; do
-  docker-compose build $name
-done
--- scripts/ip.sh
+++
@@ -1,5 +0,0 @@
-if which docker-machine >/dev/null; then
-  echo $(docker-machine ip default)
-else
-  echo $(hostname --ip-address)
-fi
