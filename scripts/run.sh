700a96330156005cd99d0b0ce24604dfe6bd394a
Tom Hanoldt <monotom@gmail.com>
2017-03-09 11:07:49 +0100

    remove ip dependency

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-IP=`./scripts/ip.sh`
-
 echo ''
-echo " -> starting wordpress web container on: $IP:9999"
+echo " -> starting wordpress web container on: 0.0.0.0:9999"
 echo ''
 
 docker-compose up
